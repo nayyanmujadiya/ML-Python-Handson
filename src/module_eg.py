77634bc67c284bd9a24f03832e2bf71ad8f7a77e
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-11 04:57:37 +0530

    import eg

---
+++ src/module_eg.py
@@ -0,0 +1,19 @@
+import math
+print(math.pi)
+
+from math import pi
+
+# Note that in the above example,
+# we used math.pi. Here we have used
+# pi directly.
+print(pi)
+
+from math import *
+print(pi)
+print(factorial(6))
+
+'''
+import mathematics
+print(mathematics.pi)
+'''
+
