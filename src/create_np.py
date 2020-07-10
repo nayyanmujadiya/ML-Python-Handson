4bb3399bff5538c1c87b2bb6a56ccb37df9cdad3
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-10 17:09:03 +0530

    create np array

---
+++ src/create_np.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+data1 = [6, 7.5, 8, 0, 1]
+
+arr1 = np.array(data1)
+
+print(arr1)
+
+data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+arr2 = np.array(data2)
+
+print(arr2)
+
+print(arr2.ndim)
+print(arr2.shape)
+
+print(arr1.dtype)
+print(arr2.dtype)
