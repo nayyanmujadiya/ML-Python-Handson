368f29b378a8535be6a9fae595d34b0179457dbf
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-10 06:22:35 +0530

    simple np vs list program

---
+++ src/list_to_np.py
@@ -0,0 +1,19 @@
+#this is NumPy example
+import numpy as np
+
+cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
+
+C = np.array(cvalues)
+
+print(C)
+
+#Let's assume, we want to turn the values into degrees Fahrenheit.
+
+print(C * 9 / 5 + 32)
+
+print(C)
+print(type(C))
+#pure python implementation
+fvalues = [ x*9/5 + 32 for x in cvalues]
+print(fvalues)
+
