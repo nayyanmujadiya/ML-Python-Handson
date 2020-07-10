307171f139f96ebd30f8ddfe9e4e18c69663b047
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-10 17:27:34 +0530

    empty0s1s

---
+++ src/emptyzerosones.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print(np.zeros(10))
+print(np.zeros((3, 6)))
+print(np.empty((2, 3, 2)))
+
+#arange is an array-valued version of the built-in Python range function
+
+print(np.arange(15))
