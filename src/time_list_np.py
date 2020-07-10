e9d72409a8d0780a89d15bab45a9f266ec0bf535
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-10 15:41:16 +0530

    Delete time_list_np.py

--- src/time_list_np.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-my_arr = np.arange(1000000)
-my_list = list(range(1000000))
-
-time for _ in range(10): my_arr2 = my_arr * 2
-time for _ in range(10): my_list2 = [x * 2 for x in my_list]
