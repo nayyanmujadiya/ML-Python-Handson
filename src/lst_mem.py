d1c220a04d6ee74cab65e6b35ed6314a7a117847
Nayyan Mujadiya <36045671+nayyanmujadiya@users.noreply.github.com>
2020-07-10 07:27:48 +0530

    lst mem

---
+++ src/lst_mem.py
@@ -0,0 +1,33 @@
+from sys import getsizeof as size
+
+lst = [24, 12, 57]
+
+size_of_list_object = size(lst)   # only green box
+size_of_elements = len(lst) * size(lst[0]) # 24, 12, 57
+
+total_list_size = size_of_list_object + size_of_elements
+print("Size without the size of the elements: ", size_of_list_object)
+print("Size of all the elements: ", size_of_elements)
+print("Total size of list, including elements: ", total_list_size)
+
+# add one more element
+#'''
+lst = [24, 12, 57, 42]
+
+size_of_list_object = size(lst)   # only green box
+size_of_elements = len(lst) * size(lst[0]) # 24, 12, 57, 42
+
+total_list_size = size_of_list_object + size_of_elements
+print("Size without the size of the elements: ", size_of_list_object)
+print("Size of all the elements: ", size_of_elements)
+print("Total size of list, including elements: ", total_list_size)
+
+lst = []
+print("Emtpy list size: ", size(lst))
+#'''
+
+'''
+We can conclude from this that for every new element, we need another eight bytes for the reference to the new object. The new integer object itself consumes 28 bytes. The size of a list "lst" without the size of the elements can be calculated with:
+
+64 + 8 * len(lst)
+'''
